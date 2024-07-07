3efd64fad4571f6dfdc4dbe3918879b09ba192c3
jawadbri <jawadalberri@gmail.com>
2024-07-07 16:04:17 +0300

    saving SignIn data to dataBase

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,18 +1,19 @@
 import '../App.css'
-import { Link } from "react-router-dom"
-
+import { Link } from 'react-router-dom'
 
 const Auth = () => {
-
-
   return (
     <div className="auth">
       Hi I'm auth
-      <form className='sign-form'>
+      <form className="sign-form">
         <h4>do you have account?</h4>
-        <Link to="/auth/logIn"><button>LogIn</button></Link>
+        <Link to="/auth/logIn">
+          <button>LogIn</button>
+        </Link>
         <h4>you don't have account?</h4>
-        <Link to="/auth/signUp"><button>SignUp</button></Link>
+        <Link to="/auth/register">
+          <button>register</button>
+        </Link>
       </form>
     </div>
   )
