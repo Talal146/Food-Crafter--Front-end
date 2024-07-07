1bffe8d53f8c0658479e492121a3cc4d10a2ca8b
jawadbri <jawadalberri@gmail.com>
2024-07-07 15:23:43 +0300

    signIn function

---
+++ src/services/Auth.js
@@ -0,0 +1,10 @@
+import Client from './api'
+export const SignInUser = async (data) => {
+  try {
+    const res = await Client.post('/auth/login', data)
+    localStorage.setItem('token', res.data.token)
+    return res.data.user
+  } catch (error) {
+    throw error
+  }
+}
