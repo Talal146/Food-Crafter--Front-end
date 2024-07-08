a06bb5b5efc998568e96818e4458e18a6cd72bb1
m00hammed <m00hammed776@gmail.com>
2024-07-08 14:57:39 +0300

    fixing categories error

--- src/pages/RecipesList.jsx
+++ src/pages/RecipesList.jsx
@@ -1,12 +1,40 @@
 import '../App.css'
 import RecipeCard from '../components/RecipeCard'
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { useParams } from "react-router-dom"
 
 const RecipesList = () => {
+  const [recipes, setRecipes] = useState([])
+  const { id } = useParams()
+
+  useEffect(() => {
+    const getRecipes = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:3001/categories/${id}/recipesList`
+        )
+
+        setRecipes(res.data.recipe)
+      } catch (err) {
+        console.log("Error fetching recipes:", err)
+      }
+    }
+    getRecipes(id)
+  }, [id])
 
 
   return (
     <div className="recipes-list">
       Recipes List
+
+      {recipes.map((recipe) => (
+        <RecipeCard
+          key={recipe._id}
+          id={recipe._id}
+          name={recipe.name}
+        />
+      ))}
       <RecipeCard />
       <RecipeCard />
       <RecipeCard />
