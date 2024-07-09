859d02c6d0c05eecf6eed263962fbce152d8961e
m00hammed <m00hammed776@gmail.com>
2024-07-09 10:42:05 +0300

    I done successfully with creatig slider for the categories

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,11 +1,13 @@
 import '../App.css'
 import { Link } from "react-router-dom"
 
+
+
 const CategoryCard = ({name, image, id}) => {
 
 
   return (
-
+   
     <Link to={`/${name}`}>
       <div className="category-card" id={id} key={id}>
         <img src={image} width='150px'/>
